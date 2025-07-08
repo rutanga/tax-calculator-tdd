d433b4da31d8cdcef9a4ad2a9abf197f02035f88
rutanga <phiphirutanga@gmail.com>
2025-07-08 16:55:46 +0300

    Second test: £5000 earnings = £0 tax (no tax bracket)

--- test.js
+++ test.js
@@ -5,4 +5,8 @@ describe('Tax Calculator', function () {
     it('should return 0 tax for earnings of £0', function () {
         assert.equal(calculateTax(0), 0);
     });
+
+    it('should return 0 tax for earnings of £5000', function () {
+        assert.equal(calculateTax(5000), 0);
+    });
 });
