e9b9710f67b2927b97a203440fe66a257173daa4
rutanga <phiphirutanga@gmail.com>
2025-07-08 17:02:17 +0300

    Sixth test: £13000 earnings = £200 tax (20% bracket)

--- tax-calculator.js
+++ tax-calculator.js
@@ -1,5 +1,11 @@
 function calculateTax(earnings) {
-    return 0;
+    if (earnings <= 12000) {
+        return 0;
+    }
+
+    // 20% tax on earnings above £12000
+    const taxableAmount = earnings - 12000;
+    return taxableAmount * 0.20;
 }
 
 module.exports = calculateTax;
