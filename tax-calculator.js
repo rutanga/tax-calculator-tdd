430a478323d24dbb29e8ac3cc97c25c41d4065c3
rutanga <phiphirutanga@gmail.com>
2025-07-08 17:11:50 +0300

    Nineth test: £37000 earnings = £5200 tax (40% bracket)

--- tax-calculator.js
+++ tax-calculator.js
@@ -3,9 +3,16 @@ function calculateTax(earnings) {
         return 0;
     }
 
-    // 20% tax on earnings above £12000
-    const taxableAmount = earnings - 12000;
-    return taxableAmount * 0.20;
+    if (earnings <= 36000) {
+        // 20% tax on earnings above £12000
+        const taxableAmount = earnings - 12000;
+        return taxableAmount * 0.20;
+    }
+
+    // 40% bracket: earnings > £36000
+    const taxAt20Percent = (36000 - 12000) * 0.20; // £4800
+    const taxAt40Percent = (earnings - 36000) * 0.40;
+    return taxAt20Percent + taxAt40Percent;
 }
 
 module.exports = calculateTax;
